4497e5915a3fa7554e93d950c384ec542ab6157d
pythiateam <pythiateam@gmail.com>
2016-08-06 21:44:15 +0500

    Добавлена функция запрашивающая разрешение на авторизацию от сервера

--- blank.py
+++ blank.py
@@ -7,12 +7,11 @@
 import serial
 import socket
 import constants
-#from __future__ import print_function
-
-# функция считывания данных с последовательного порта
+# from __future__ import print_function
 
 
 def read_card_data_from_serial():
+    # функция считывания данных с последовательного порта
         data_from_serial = ""
         current_char = ''
         serial_reader = serial.Serial("/dev/ttyUSB0", baudrate=9600)
@@ -22,21 +21,30 @@ def read_card_data_from_serial():
         serial_reader.close()
         return data_from_serial
 
-# TODO - написать функцию-парсер, возвращающую идентификатор карты
-
 
 def parse_id_from_card_data(card_data):
+    # функция-парсер, возвращающая строку с идентификатор карты или пустую строку в случае неудачи
     try:
         second_block = card_data.split(',')[1]
+        return second_block.split()[2]
     except IndexError:
-        return 0
-    try:
-        return int(second_block.split()[2])
-    except ValueError:
-        return 0
+        return ''
 
-# TODO - написать функцию отправляющую запрос об идентификаторе на сервер и получающую ответ
-#  (в виде заглушки, с авторизацией по моей карте)
+
+def request_permission_for_auth(card_id):
+    # функция отправляющая запрос на разрешение аутентификации на сервер
+    # и получающую ответ DENY, ALLOW или 'NETWORK_ERROR'
+    # (в виде заглушки, с авторизацией по моей карте)
+    try:
+        arm_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        arm_socket.connect((constants.SERVER_HOST, constants.PORT))
+        arm_socket.sendall(card_id)
+        data = arm_socket.recv(1024)
+    except socket.error:
+        return 'NETWORK_ERROR'
+    finally:
+        arm_socket.close()
+    return data
 
 # TODO - написать функцию, которая авторизует пользователя (или нет)
 
