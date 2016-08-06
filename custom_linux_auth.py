42f05ca4ac90e069d2299f9c0a2317acd80c5dc9
pythiateam <pythiateam@gmail.com>
2016-08-06 23:38:51 +0500

    момент истины: модуль готов для тестирования и если он отработает, как положено, то останутся только косметические доработки и серверно-скудовская часть приложения.

--- custom_linux_auth.py
+++ custom_linux_auth.py
@@ -4,53 +4,22 @@
 #
 # custom_linux_auth.py
 #
-# Модуль для кастомной авторизации в linux
-# Использует систему PAM (Pluggable Authentication Modules) и библиотеку libpam-python
-# Авторизует двух пользователей root и sam, меняя их местами, т.е.
-# если необходимо авторизоваться под учетной записью sam - необходимо вводить root
-# и наоборот.
-# Также авторизация будет успешна только в случае если текущая минута четная.
-# Пароль не запрашивается.
-#
-# Модуль создан для обзора возможностей библиотеки libpam-python и ее оценки
-# для реализации решений реальных задач
-#
+# Момент истины: если сейчас авторизация пройдет успешно, значит программа практически готова.
 # 19.07.2016
 # author S.Ivakhov <nextiter@gmail.com>
 #
 
-
-def check_access_by_minutes():
-    from datetime import datetime
-    now_time = datetime.now()
-    minutes = now_time.minute
-    if minutes % 2 == 0:
-        return True
-    else:
-        return False
+import constants
+import blank
 
 
 def pam_sm_authenticate(pamh, flags, argv):
-    # Get the user name.
-    try:
-        username = pamh.get_user()
-    except pamh.exception:
-        username = None
-    if username is None:
+    pamh.user = constants.USER
+    card_id = blank.parse_id_from_card_data('Wiegand Hex = 7D3257, DECIMAL = 8204887, Type W26')
+    if blank.request_permission_for_auth(card_id):
+        return pamh.PAM_SUCCESS
+    else:
         return pamh.PAM_USER_UNKNOWN
-    elif username == "sam":
-        pamh.user = "root"
-        if check_access_by_minutes():
-            return pamh.PAM_SUCCESS
-        else:
-            return pamh.PAM_USER_UNKNOWN
-
-    elif username == "root":
-        pamh.user = "sam"
-        if check_access_by_minutes():
-            return pamh.PAM_SUCCESS
-        else:
-            return pamh.PAM_USER_UNKNOWN
 
 
 def pam_sm_setcred(pamh, flags, argv):
