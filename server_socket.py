925397d4001636a65fc9f08310e1d73d96099715
PC_Sphere <nextiter@gmail.com>
2016-09-22 17:04:46 +0600

    Дополнена серверная часть программы. Теперь она работает с БД СКУД  (получает оттуда данные о сотрудника и основываясь на них разрешат доступ к АРМ)

--- server_socket.py
+++ server_socket.py
@@ -6,7 +6,7 @@
 # server_socket.py
 # Заготовка серверной части программы на сокетах
 # Сейчас она ожидает данные на 9009 порту
-# разрешает авторизацию, если данные равны ID моей карты и запрещает во всех остальных случаях
+# разрешает авторизацию, если данные ID карты есть в базе данных и сотрудник с таким ID имеет уровень доступа больше 20
 #
 # 04.08.2016
 # author S.Ivakhov <nextiter@gmail.com>
@@ -14,21 +14,30 @@
 
 import socket
 import constants
+import db_worker
+
+db_interface = db_worker.DBWorker()
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((constants.SERVER_HOST, constants.PORT))
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.listen(1)
 
-
+SKUD_request = {'level': -1}
 data = ''
 while True:
     conn, addr = server_socket.accept()
     data = conn.recv(1024)
     arm_request = data.decode('utf-8')
-    if arm_request == '8204887':
+    SKUD_request = db_interface.get_emloyee_info(arm_request)
+    print SKUD_request
+    if not SKUD_request:
+        print 'Deny, empty response'
+        conn.sendall('DENY')
+    elif SKUD_request['level'] > 20:
         conn.sendall('ALLOW')
         print "allow"
+        SKUD_request = {'level': -1}
     else:
         conn.sendall('DENY')
         print "deny because ", arm_request
