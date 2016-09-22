e8910843cf48fde08585377e463842423846592f
PC_Sphere <nextiter@gmail.com>
2016-09-22 15:26:03 +0600

    Дополнен файл с константами

--- constants.py
+++ constants.py
@@ -1,4 +1,9 @@
 SERVER_HOST = "192.168.0.37"
 CLIENT_HOST = "192.168.0.244"
 PORT = 9009
-USER = 'use1'
+USER = 'user1'
+
+DB_USER = "sysdba"
+DB_PASSWORD = "masterkey"
+DB_HOST = "localhost"
+DATABASE = "C:\\SSA\SKD\\codos_db\\codos.gdb"
